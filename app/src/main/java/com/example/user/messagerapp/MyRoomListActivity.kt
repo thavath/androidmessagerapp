37ad3853c7ed2ea601c3b8b351a697282dffd9a4
Rath Thavath <thavathrath@gmail.com>
2018-12-25 18:02:34 +0700

    Initial commit

--- app/src/main/java/com/example/user/messagerapp/MyRoomListActivity.kt
+++ app/src/main/java/com/example/user/messagerapp/MyRoomListActivity.kt
@@ -1,7 +1,10 @@
 package com.example.user.messagerapp
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MyRoomListActivity : AppCompatActivity() {
 
@@ -9,5 +12,26 @@ class MyRoomListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_room_list)
         supportActionBar?.title = "Room List"
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.add_building, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when(item?.itemId){
+            R.id.add_building -> {
+                var intent = Intent(this, AddNewBuildingActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.add_new_room -> {
+                var intent = Intent(this, AddRoomActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
